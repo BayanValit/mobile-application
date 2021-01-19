659f3c3236cabc34683a24116150162a200edccb
Neo <knyazevi2339@gmail.com>
2021-01-20 02:37:30 +0700

    Replaced ListView to RecyclerView

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,8 +2,11 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -38,7 +41,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = new Sandwich();;
+        Sandwich sandwich = new Sandwich();
         try {
             sandwich = JsonUtils.parseSandwichJson(json);
         } catch (JSONException e) {
@@ -50,7 +53,7 @@ public class DetailActivity extends AppCompatActivity {
             }
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +66,16 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView decription = findViewById(R.id.description_tv);
+        TextView placeoforigin = findViewById(R.id.origin_tv);
 
+
+        decription.setText(sandwich.getDescription());
+        placeoforigin.setText(sandwich.getPlaceOfOrigin());
+
+        RecyclerView rv2 = findViewById(R.id.ingredients_tv2);
+        rv2.setLayoutManager(new LinearLayoutManager(this));
+        rv2.setAdapter(new RViewAdapter2(this, sandwich.getIngredients()));
     }
 }
