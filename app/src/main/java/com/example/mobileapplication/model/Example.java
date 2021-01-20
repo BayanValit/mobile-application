47a89174613aae9268dac2ec5f7889eef2d27524
Neo <knyazevi2339@gmail.com>
2021-01-21 01:15:06 +0700

    Added objects classes

---
+++ app/src/main/java/com/example/mobileapplication/model/Example.java
@@ -0,0 +1,24 @@
+package com.example.mobileapplication.model;
+
+public class Example {
+
+    private Photo photos;
+
+    private String stat;
+
+    public Photo getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photo photos) {
+        this.photos = photos;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+}
