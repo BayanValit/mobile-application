051818e8ff0c4c8f3cdb2623b759faa77d115c97
Neo <knyazevi2339@gmail.com>
2021-01-21 01:35:17 +0700

    Added RecyclerView

--- app/src/main/java/com/example/mobileapplication/PhotoGallery.java
+++ app/src/main/java/com/example/mobileapplication/PhotoGallery.java
@@ -1,8 +1,10 @@
 package com.example.mobileapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PhotoGallery extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class PhotoGallery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_activity);
+
+        final RecyclerView rv = findViewById(R.id.rView);
+        rv.setLayoutManager(new GridLayoutManager(this, 3));
     }
 }
